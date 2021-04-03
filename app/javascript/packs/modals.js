dd588e747d5e24897ac00a394d59395e9c53aa41
Nilomiranda <nilomiranda3@gmail.com>
2021-04-02 21:35:32 -0300

    added modal component
    :

--- app/javascript/packs/modals.js
+++ app/javascript/packs/modals.js
@@ -3,7 +3,21 @@ document.addEventListener('DOMContentLoaded', () => {
         const $modal = $modalClose.parentNode;
 
         $modalClose.addEventListener('click', () => {
-            $modal.parentNode.removeChild($modal);
+            $modal.classList.remove('is-active');
         });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    (document.querySelectorAll('.modal-btn') || []).forEach(($modalBtn) => {
+        $modalBtn.addEventListener('click', () => {
+            console.log($modalBtn.dataset.modal)
+
+            const $targetModal = document.querySelector(`#${$modalBtn.dataset.modal}`)
+
+            if ($targetModal) {
+                $targetModal.classList.add('is-active')
+            }
+        })
     });
 });
