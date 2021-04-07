80ce4004a3121d77fca6bf997029013b435feec7
Nilomiranda <nilomiranda3@gmail.com>
2021-04-06 22:34:26 -0300

    migrated to add tweet_likes

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_03_000036) do
+ActiveRecord::Schema.define(version: 2021_04_07_013212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tweet_likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "tweet_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["tweet_id"], name: "index_tweet_likes_on_tweet_id"
+    t.index ["user_id"], name: "index_tweet_likes_on_user_id"
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.string "content"
@@ -37,5 +46,7 @@ ActiveRecord::Schema.define(version: 2021_04_03_000036) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tweet_likes", "tweets"
+  add_foreign_key "tweet_likes", "users"
   add_foreign_key "tweets", "users"
 end
